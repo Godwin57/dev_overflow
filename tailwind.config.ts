2998c59fc58921fb408758b27466c542e79ec6ca
Godwin Emeribe <godwinemeribe23@gmail.com>
2024-03-21 14:04:56 +0100

    Updated the css files theme.css and  globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,5 +74,8 @@ module.exports = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate"), require("@tailwindcss/")],
+    plugins: [
+        require("tailwindcss-animate"),
+        require("@tailwindcss/typography"),
+    ],
 };
